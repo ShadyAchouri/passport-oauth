81372b979dd2139ad056afa13de5a14350ff98cb
Shady Achouri <shady.achouri@gmail.com>
2020-12-18 12:22:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+
+var auth = require('./routes/auth');
+
+var app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/mean-social', {useNewUrlParser: true})
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+app.use(session({
+  secret: 's3cr3t',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ auth/google.js
@@ -0,0 +1,17 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var User = require('../models/User');
+
+passport.use(new GoogleStrategy({
+    clientID: "142336991925-ssdf8cdhltbl9thl8u89lv43c91kbjfh.apps.googleusercontent.com",
+    clientSecret: "Gm8Fx_fgyAc4SrtBTrNDSaew",
+    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
+
+module.exports = passport;
